6cdc2a7f907c0aecd79d1a4a40bd3c6557ff861a
zaki031 <m.zakariahamoudi@gmail.com>
2023-07-30 14:50:49 +0100

    getting data from form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,15 +1,62 @@
-import React from 'react'
-import {BsCaretDownFill} from "react-icons/bs"
+import React, { useState } from 'react';
+import Swal from 'sweetalert2'
+
 const Form = () => {
 
-  const handleSubmit = () => {
-    console.log("test");
+  const [formData, setFormData] = useState({
+    
+    fullName: '',
+    discordID:'',
+    email: '',
+    phoneNumber: '',
+    firstField: '',
+    second_Field: '',
+  });
+
+
+
+  const [firstField, setfirstField] = useState('');
+  const [secondField, setsecondField] = useState('');
+
+  // Step 3: Event handler to capture the selected value
+  const handleFirstFieldInputChange = (event) => {
+    const value = event.target.value;
+    setfirstField(value);
+  };
+  const handlesecondFieldInputChange = (event) => {
+    const value = event.target.value;
+    setsecondField(value);
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({
+      fullName: e.target.elements.fullname.value,
+      discordID: e.target.elements.discord.value,
+      email: e.target.elements.email.value,
+      phoneNumber:e.target.elements.phone.value,
+      firstField:firstField,
+      second_Field:secondField,
+
+
+    });
+    Swal.fire(
+      'Good job!',
+      'Your Submission has been sent!',
+      'success',
+
+    )
+    
+    console.log(formData);
   };
   return (
+
+    
     <div>
 
 
-<form class="form-grid" onSubmit={handleSubmit()}>
+<form class="form-grid" onSubmit={handleSubmit} >
     <div class="form-group">
       <div className='firstname'>
       <label for="fullname"  >Full Name <span>*</span></label>
@@ -42,13 +89,14 @@ const Form = () => {
     <div class="form-group">
       <div className='fields'>
         <label for="training">First Training Field <span>*</span></label>
-        <select>
+        <select name="firstField" value={firstField} onChange={handleFirstFieldInputChange}>
   <option selected value="0">Select your first field</option>
   <option value="1">Web Dev</option>
   <option value="2">Introductionto Ai and Data Science</option>
   <option value="3">3D deisgn </option>
-  <option value="3">Graphic Design Theory </option>
-  <option value="3">Introduction to UI/UX Design</option>
+  <option value="4">Graphic Design Theory </option>
+  <option value="5">Introduction to UI/UX Design</option>
+  <option value="6">Linux</option>
 
 </select>
 
@@ -58,14 +106,14 @@ const Form = () => {
     <div class="form-group">
       <div className='fields'>
         <label for="training">Second Training Field <span>*</span></label>
-      <select>
+      <select name='secondField' value={secondField} onChange={handlesecondFieldInputChange}>
   <option selected value="0">Select your second field</option>
   <option value="1">Web Dev</option>
   <option value="2">Introductionto Ai and Data Science</option>
   <option value="3">3D deisgn </option>
-  <option value="3">Graphic Design Theory </option>
-  <option value="3">Introduction to UI/UX Design</option>
-
+  <option value="4">Graphic Design Theory </option>
+  <option value="5">Introduction to UI/UX Design</option>
+  <option value="6">Linux</option>
 </select>
 
       </div>
@@ -75,6 +123,7 @@ const Form = () => {
       <button type="submit">Register</button>
     </div>
   </form>
+
 
 
     
