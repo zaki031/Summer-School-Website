dac2c4241fb048f2f471035dd87a243ab8e57760
Zakaria <115791170+zaki031@users.noreply.github.com>
2023-07-31 20:49:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -55,12 +55,12 @@ function App() {
     
     <div className='f-p' >    <p >
     Open Minds est un club scientifique estudiantin de l'USTHB crée en 2009.<br/>
-OMC  est le seul représentant Algérien, leader au sein du réseau "CLEF".<br/>
+Omc  est le seul représentant Algérien, leader au sein du réseau "CLEF".<br/>
 Son but est de promouvoir le libre open source en Algérie et cultiver l'entraide étudiante.<br/>
 
-<p id='slogon'><b>Se qui ne se partage pas se perd</b></p>
+<p id='slogon'><b>Ce Qui Ne Se Partage Pas Se Perd</b></p>
 
-L'OMC ne se limite pas à être une simple organisation, c'est une famille unie où chaque membre a l'opportunité de vivre des expériences uniques et enrichissantes.<br/>
+L'Omc ne se limite pas à être une simple organisation, c'est une famille unie où chaque membre a l'opportunité de vivre des expériences uniques et enrichissantes.<br/>
 
     </p>
 </div>
